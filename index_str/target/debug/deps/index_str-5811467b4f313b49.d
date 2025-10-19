f659956a1fc460ed4ef0309410488663facc6dd9
Danylo Serkiz <danylo.serkiz@globallogic.com>
2025-10-19 17:29:34 +0300

    index_str

---
+++ index_str/target/debug/deps/index_str-5811467b4f313b49.d
@@ -0,0 +1,5 @@
+/home/danylo/Desktop/Rust/index_str/target/debug/deps/index_str-5811467b4f313b49.d: src/main.rs
+
+/home/danylo/Desktop/Rust/index_str/target/debug/deps/libindex_str-5811467b4f313b49.rmeta: src/main.rs
+
+src/main.rs:
