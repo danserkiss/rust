6433e62fd119854ca288a0b18c4f338193f58ea4
Danylo Serkiz <danylo.serkiz@globallogic.com>
2025-11-12 15:59:25 +0200

    feat: add thread pool
    - implement thread pool
    - change Display for MyMap
    - capture result to "index_result.json"
    - add 'max_th' parameter to program arguments

--- index_str/src/main.rs
+++ index_str/src/main.rs
@@ -2,8 +2,10 @@ use clap::Parser;
 use std::collections::HashMap;
 use std::fmt;
 use std::fs;
-use std::sync::mpsc::{self};
-use std::thread;
+use std::fs::File;
+use std::io::Write;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -11,6 +13,8 @@ struct Args {
     //Path to file
     #[arg(short, long)]
     path: String,
+    #[arg(short, long)]
+    max_th: usize,
 }
 
 struct MyMap {
@@ -35,10 +39,9 @@ impl MyMap {
 impl fmt::Display for MyMap {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for (word, map) in self.map.iter() {
-            let _ = write!(f, "{word}:{{\n");
+            let _ = write!(f, "\"{word}\":{{\n");
             for (file, indx) in map.iter() {
-                let _ = write!(f, "{file}: {indx:?}\n");
-                //println!("map:{map:?}\n");
+                let _ = write!(f, "\"{file}\": {indx:?},\n");
             }
             let _ = write!(f, "}}\n");
         }
@@ -48,55 +51,87 @@ impl fmt::Display for MyMap {
 
 fn main() {
     let args = Args::parse();
-    parse(&args.path);
+    let mut path_buf: Vec<String> = Vec::new();
+    collet_files(&args.path, &mut path_buf);
+    let tasks = Arc::new(Mutex::new(path_buf.into_iter()));
+    let result = Arc::new(Mutex::new(Vec::new()));
+
+    std::thread::scope(|scope| {
+        let mut handles = Vec::new();
+        for _ in 0..args.max_th {
+            let tasks_cl = Arc::clone(&tasks);
+            let result_cl = Arc::clone(&result);
+            let handle = scope.spawn(move || {
+                let mut th_res = Vec::new();
+                loop {
+                    let next_task = {
+                        let mut task_guard = tasks_cl.lock().unwrap();
+                        task_guard.next()
+                    };
+                    match next_task {
+                        Some(task_data) => {
+                            let result = index_file(task_data.as_str());
+                            th_res.push(result);
+                        }
+                        None => {
+                            break;
+                        }
+                    }
+                }
+                let mut result_guard = result_cl.lock().unwrap();
+                result_guard.extend(th_res);
+            });
+            handles.push(handle);
+        }
+        for handle in handles {
+            handle.join().unwrap();
+        }
+    });
+
+    let mut mymap: MyMap = MyMap::new();
+    for i in result.lock().unwrap().iter() {
+        mymap.merge(i.map.clone());
+    }
+    let mut file = File::create("index_result.json").unwrap();
+    println!("{}", mymap);
+    let _ = writeln!(file, "{}", mymap);
 }
-
-fn parse(path: &String) {
-    let files = fs::read_dir(path);
-    if files.is_ok() {
+fn collet_files(path: &String, path_buf: &mut Vec<String>) {
+    let directory = fs::read_dir(path);
+    if directory.is_ok() {
         //dir
-        let mut threads = vec![];
-        let mut mymap = MyMap::new();
-        let (tx, rx) = mpsc::channel();
-
-        for i in files.unwrap() {
+        for i in directory.unwrap() {
             if let Ok(file) = i {
                 let filename_opt = file.file_name().clone();
                 let filename = filename_opt.into_string().unwrap();
                 let mut fullpath = path.clone();
                 fullpath.push('/');
                 fullpath.push_str(&filename);
-
-                let thread_tx = tx.clone();
-                threads.push(thread::spawn(move || {
-                    let mut mymap_th = MyMap::new();
-                    mymap_th = read_file(&fullpath, &filename, mymap_th);
-                    let _ = thread_tx.send(mymap_th.map);
-                }));
+                if let Ok(filetype) = file.file_type() {
+                    if filetype.is_dir() {
+                        let mut rec_vec = Vec::new();
+                        collet_files(&fullpath, &mut rec_vec);
+                        path_buf.append(&mut rec_vec);
+                    } else if filetype.is_file() {
+                        path_buf.push(fullpath);
+                    }
+                }
             }
         }
-        drop(tx);
-        for recived_map in rx {
-            mymap.merge(recived_map);
-        }
-        for th in threads {
-            let _ = th.join();
-        }
-
-        println!("{}", mymap);
     } else {
         //file
-        let mut mymap = MyMap::new();
-        mymap = read_file(&path, &path, mymap);
-        println!("{}", mymap);
+        path_buf.push(path.clone());
     }
 }
 
-fn read_file(path: &str, filename: &str, mut mymap: MyMap) -> MyMap {
+fn index_file(path: &str) -> MyMap {
+    let mut mymap = MyMap::new();
+    let vec: Vec<&str> = path.rsplit('/').collect();
+
     let contents = fs::read_to_string(&path); // read from file to str
     if contents.is_ok() {
         let text = contents.unwrap();
-        mymap = index_words(&text, filename, mymap);
+        mymap = index_words(&text, vec[0], mymap);
     }
     return mymap;
 }
